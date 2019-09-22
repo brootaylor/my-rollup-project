54396e93dae1490d5e7b319dd35098de52035137
Bruce Taylor <bruce.taylor@avios.com>
2019-09-22 12:54:19 +0100

    Some JS export changes and helps.

---
+++ src/components/core/menu/menu.js
@@ -0,0 +1,11 @@
+// components/core/menu/menu.js
+
+// Default funcrion for the menu.js
+export default function () {
+    'Menu default function!'
+}
+
+// Another function that can be called. What is this type of function / mehtod called????
+export function dropDown () {
+	console.log("Dropdown function in the menu.js file.");
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,23 @@
-// src/main.js
+// src/scripts/main.js
 import { version } from '../../package.json';
 import answer from 'the-answer';
 import foo from './modules/foo.js';
 import bar from './modules/foo.js';
 import { square } from './modules/maths.js';
 
+// src/components/.../*.js
+import menu, { dropDown } from '../components/core/menu/menu.js'; // Daisy-chained default and specific function/s.
+
+
 export default function () {
     console.log('version ' + version);
 
     console.log(foo);
     console.log(bar);
+
+    // Functions from the menu.js
+    console.log(menu);
+    console.log(dropDown);
 
     // COMMENTED OUT FOR NOW => Because UMD and IIFE output formats are not supported for code-splitting builds.
     // ======================
